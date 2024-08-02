1179ee6c16cfc61faeb3f07c440b5c3a1991aebe
Kalinkin Yaroslav <kalinkin.y.o@mail.ru>
2024-08-02 18:59:22 +0300

    nice try

--- some_file.py
+++ some_file.py
@@ -1,4 +1,53 @@
 from typing import Literal
+from dataclasses import dataclass
+
+
+class User:
+    def __init__(self, user_id, name, age, email):
+        self.user_id = user_id
+        self.name = name
+        self.age = age
+        self.email = email
+
+
+# То же самое что и класс выше
+@dataclass
+class User1:
+    user_id: int
+    name: str
+    age: int
+    email: str
+
+
+@dataclass
+class DatabaseConfig:
+    db_host: str       # URL-адрес базы данных
+    db_user: str       # Username пользователя базы данных
+    db_password: str   # Пароль к базе данных
+    database: str      # Название базы данных
+
+
+@dataclass
+class TgBot:
+    token: str             # Токен для доступа к телеграм-боту
+    admin_ids: list[int]   # Список id администраторов бота
+
+
+@dataclass
+class Config:
+    tg_bot: TgBot
+    db: DatabaseConfig
+
+
+def get_user_info(user: User1 | User) -> str:
+    return f'Возраст пользователя {user.name} - {user.age}, ' \
+           f'а email - {user.email}'
+
+
+user_2: User = User(15, 'Ivan', 35, 'ivan_pupkin@pochta.ru')
+print(get_user_info(user_2))
+user_1: User1 = User1(42, 'Vasiliy', 23, 'vasya_pupkin@pochta.ru')
+print(get_user_info(user_1))
 
 
 def test_fun(number: int | float = 1, name: str = 'Ivan') -> str:
