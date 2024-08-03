ab57b46e828dc338acda984259efd0118af01409
Kalinkin Yaroslav <kalinkin.y.o@mail.ru>
2024-08-03 03:17:13 +0300

    Test Api code 1.1

---
+++ TgBotApi.py
@@ -0,0 +1,17 @@
+import requests
+
+token = "7445355602:AAG7JbbH5l2YkUZ7j-q9erG0iVty4UJqXYY"
+api_url = f'https://api.telegram.org/bot{token}/getMe'
+
+# Прмер получения API
+# https://api.telegram.org/bot<token>/METHOD_NAME
+# Отправляем GET-запрос и сохраняем ответ в переменной response
+response = requests.get(api_url)
+
+print(api_url)
+# Если код ответа на запрос - 200, то смотрим, что пришло в ответе
+if response.status_code == 200:
+    print(response.text)
+# При другом коде ответа выводим этот код
+else:
+    print(response.status_code)
